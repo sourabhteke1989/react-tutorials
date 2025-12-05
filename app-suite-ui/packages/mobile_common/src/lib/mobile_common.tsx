df095cbef283cf7c40cb3a0398bb7bc0e01e7f1f
sourabhteke <sourabh.teke@gmail.com>
2025-12-05 23:19:09 +0530

    Using common library into mobile-common library project

--- app-suite-ui/packages/mobile_common/src/lib/mobile_common.tsx
+++ app-suite-ui/packages/mobile_common/src/lib/mobile_common.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { common } from '@app_suite/common';
 
 /* eslint-disable-next-line */
 export interface MobileCommonProps {}
 
 export function MobileCommon(props: MobileCommonProps) {
+  const str = common();
   return (
     <View>
-      <Text>Welcome to mobile_common!</Text>
+      <Text>Welcome to mobile_common! Message from common library [{str}]</Text>
     </View>
   );
 }
